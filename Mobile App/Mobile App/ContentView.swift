a345000073ee2272b3a43e424b5fd44bb5245607
Shuo Wang <s.wang970602@gmail.com>
2020-05-07 10:32:02 +0800

    Create the project

---
+++ Mobile App/Mobile App/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mobile App
+//
+//  Created by Shuo Wang on 7/5/20.
+//  Copyright © 2020 Shuo Wang. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
