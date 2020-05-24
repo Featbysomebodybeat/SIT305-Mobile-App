1d582a5e1d247b9cd60631e51e7af470d0a7affb
Shuo Wang <s.wang970602@gmail.com>
2020-05-24 15:08:01 +0800

    Recreat

---
+++ mab/mab/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mab
+//
+//  Created by Shuo Wang on 24/5/20.
+//  Copyright © 2020 Shuo Wang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
