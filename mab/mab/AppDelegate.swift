cf670de163474a4331e4a243d73e94c187cbf656
Shuo Wang <s.wang970602@gmail.com>
2020-06-10 18:59:28 +0800

    Update AppDelegate.swift

--- mab/mab/AppDelegate.swift
+++ mab/mab/AppDelegate.swift
@@ -9,7 +9,7 @@
 import UIKit
 
 @UIApplicationMain
-caclass AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
     
     var window: UIWindow?
     
